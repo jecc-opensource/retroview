94f4b95624d0f2d782eb41b08c45282e6abc4e1a
Chase Walters <cwwalters@fastmail.com>
2022-09-14 13:35:04 -0600

    Add interviw box, style boxes, add Link functionality

--- client/pages/uidemo.js
+++ client/pages/uidemo.js
@@ -1,5 +1,6 @@
 import Card from '../components/Card/Card';
 import SkillBox from '../components/SkillBox/SkillBox';
+import InterviewBox from '../components/InterviewBox/InterviewBox';
 
 export default function UIDemo() {
   const low = 1;
@@ -11,9 +12,14 @@ export default function UIDemo() {
       <Card title="Card Title">
       Very jealous pupper bork fat boi doggo blep smol borking doggo with a long snoot for pats, pats puggorino heckin good boys blep, long bois extremely cuuuuuute shoober dat tungg tho. Very good spot heckin good boys and girls big ol he made many woofs long bois vvv clouds, doge shooberino aqua doggo very taste wow. Doing me a frighten snoot super chub doing me a frighten, ur givin me a spook doing me a frighten. Big ol long bois much ruin diet very good spot big ol pupper long water shoob bork, blop very hand that feed shibe puggorino h*ck. Doggorino such treat length boy pupperino he made many woofs, doggo wow very biscit. mlem. Corgo doggorino long bois what a nice floof heckin angery woofer, wow such tempt h*ck heckin good boys. Super chub thicc ruff yapper lotsa pats long woofer, you are doin me a concern borkf length boy. Porgo ruff you are doin me a concern shooberino adorable doggo heckin angery woofer, adorable doggo doing me a frighten doge snoot, borking doggo stop it fren wow very biscit tungg.
       </Card>
-      <SkillBox label="Next.js" confidence={low} />
-      <SkillBox label="Redux" confidence={medium} />
+      <hr/>
+      <SkillBox label="Next.js" href="http://nextjs.org" confidence={low} />
+      <SkillBox label="Redux" href="/skills/redux" confidence={medium} />
       <SkillBox label="React" confidence={high} />
+      <hr/>
+      <InterviewBox label="Google" href="http://google.com" confidence={low} />
+      <InterviewBox label="International Consortium of Coders" confidence={medium} />
+      <InterviewBox label="CapitalOne" href="/interviews/capitalone" confidence={high} />
 
     </div>
   );
