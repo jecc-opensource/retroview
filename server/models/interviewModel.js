085cd1abd43a6b49f1108d248abff114f159000c
chrisjamali <jamalichristopher@gmail.com>
2022-09-14 10:02:51 -0700

    moved the create dir function to the models file

--- server/models/interviewModel.js
+++ server/models/interviewModel.js
@@ -1,7 +1,18 @@
+/* eslint-disable no-bitwise */
+/* eslint-disable no-console */
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 const fs = require('fs');
-const makeDatabaseDir = require('../utils/utils');
+const { ensureDirSync } = require('fs-extra');
+
+function makeDatabaseDir() {
+  try {
+    ensureDirSync(path.resolve(__dirname, '../../database'));
+    console.log('I am after EnsureDirSync');
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 makeDatabaseDir();
 
@@ -11,7 +22,7 @@ sqlite3.verbose();
 
 const dbPath = path.resolve(__dirname, '../../database/interviewDB.db');
 const dbExists = fs.existsSync(dbPath);
-//make the databases
+// make the databases
 const interviewDB = new sqlite3.Database(
   dbPath,
   sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
@@ -21,29 +32,29 @@ const interviewDB = new sqlite3.Database(
       throw err;
     }
     console.log('Connection successful to database :)');
-  }
+  },
 );
-//make tables only if the databases wasnt created yet
+// make tables only if the databases wasnt created yet
 if (!dbExists) {
-  //ensure that all sqlite actions happen in order 
+  // ensure that all sqlite actions happen in order
   interviewDB.serialize(() => {
-    //make interviews table
+    // make interviews table
     interviewDB.run(
-      `CREATE TABLE interviews(id INTEGER PRIMARY KEY AUTOINCREMENT , interview_date ,job_title TEXT NOT NULL , company TEXT NOT NULL,tech_stack TEXT , resume_version TEXT, interest_level TEXT CHECK( interest_level IN ('1','2','3') )  , questions TEXT DEFAULT NULL, notes text DEFAULT NULL )`
+      'CREATE TABLE interviews(id INTEGER PRIMARY KEY AUTOINCREMENT , interview_date ,job_title TEXT NOT NULL , company TEXT NOT NULL,tech_stack TEXT , resume_version TEXT, interest_level TEXT CHECK( interest_level IN (\'1\',\'2\',\'3\') )  , questions TEXT DEFAULT NULL, notes text DEFAULT NULL )',
     );
 
-    //make skills table
+    // make skills table
     interviewDB.run(`
 CREATE TABLE skills(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, question_prompt TEXT, answer TEXT, confidence TEXT CHECK( confidence IN ('1','2','3') ))
 `);
 
-    //make assocative entity or join table
+    // make assocative entity or join table
     interviewDB.run(
-      `CREATE TABLE interToSkill(interview_id INTEGER NOT NULL, skill_id INTEGER NOT NULL, PRIMARY KEY(interview_id, skill_id) )`
+      'CREATE TABLE interToSkill(interview_id INTEGER NOT NULL, skill_id INTEGER NOT NULL, PRIMARY KEY(interview_id, skill_id) )',
     );
 
-    //sample insert into interviews
-    let sql = `INSERT INTO interviews (interview_date, job_title,company, tech_stack, resume_version, interest_level, questions, notes) VALUES (?,?,?,?,?,?,?,?)`;
+    // sample insert into interviews
+    const sql = 'INSERT INTO interviews (interview_date, job_title,company, tech_stack, resume_version, interest_level, questions, notes) VALUES (?,?,?,?,?,?,?,?)';
 
     interviewDB.run(sql, [
       '9/13/2022',
@@ -56,8 +67,8 @@ CREATE TABLE skills(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, qu
       'No :)',
     ]);
 
-    //sample insert into skills
-    let sqlSkill = `INSERT INTO skills(name, question_prompt, answer, confidence) VALUES (?,?,?,?)`;
+    // sample insert into skills
+    const sqlSkill = 'INSERT INTO skills(name, question_prompt, answer, confidence) VALUES (?,?,?,?)';
 
     interviewDB.run(sqlSkill, [
       'React',
@@ -66,29 +77,29 @@ CREATE TABLE skills(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, qu
       '1',
     ]);
 
-    //sample insert into interToSkill join table
-    let joinInsert = `INSERT INTO interToSkill(skill_id,interview_id) VALUES(?,?)`;
+    // sample insert into interToSkill join table
+    const joinInsert = 'INSERT INTO interToSkill(skill_id,interview_id) VALUES(?,?)';
     interviewDB.run(joinInsert, [1, 1]);
 
-    //selection queries to get all the info from intervies and skills table
-    const allInterviews = `SELECT * FROM interviews`;
-    const allSkills = `SELECT * FROM skills`;
-    const allJoin = `SELECT * FROM interToSkill`;
-    
-    //console.log all the interviews
+    // selection queries to get all the info from intervies and skills table
+    const allInterviews = 'SELECT * FROM interviews';
+    const allSkills = 'SELECT * FROM skills';
+    const allJoin = 'SELECT * FROM interToSkill';
+
+    // console.log all the interviews
     interviewDB.all(allInterviews, [], (err, rows) => {
       rows.forEach((row) => {
         console.log('INTERVIEWS =>', row);
       });
     });
 
-    //console.log all the skills
+    // console.log all the skills
     interviewDB.all(allSkills, [], (err, rows) => {
       rows.forEach((row) => {
         console.log('SKILLS =>', row);
       });
     });
-    //console.log all the joins
+    // console.log all the joins
     interviewDB.all(allJoin, [], (err, rows) => {
       rows.forEach((row) => {
         console.log('JOINS =>', row);
@@ -97,7 +108,8 @@ CREATE TABLE skills(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, qu
   });
 }
 
-//best practices to close database
+// best practices to close database
 interviewDB.close((err) => {
   if (err) return console.log(err.message);
+  console.log('EXITED');
 });
