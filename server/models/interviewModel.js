c06fb2c2a9c0bee4abef2a7739c597ca398c7242
chrisjamali <jamalichristopher@gmail.com>
2022-09-14 07:53:10 -0700

    completed interviews database setup

--- server/models/interviewModel.js
+++ server/models/interviewModel.js
@@ -25,10 +25,12 @@ const interviewDB = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE | sqlite
 )
 if (!dbExists){
 interviewDB.run(
-  `CREATE TABLE interviews(id INTEGER PRIMARY KEY AUTOINCREMENT , interview_date ,job_title TEXT NOT NULL , company TEXT NOT NULL,tech_stack TEXT , resume_version TEXT, interest_level  TEXT CHECK( interest_level IN ('1','2','3') )  , questions TEXT DEFAULT NULL, notes text DEFAULT NULL )`
+  `CREATE TABLE interviews(id INTEGER PRIMARY KEY AUTOINCREMENT , interview_date ,job_title TEXT NOT NULL , company TEXT NOT NULL,tech_stack TEXT , resume_version TEXT, interest_level TEXT CHECK( interest_level IN ('1','2','3') )  , questions TEXT DEFAULT NULL, notes text DEFAULT NULL )`
 );
 
-
+interviewDB.run(`
+CREATE TABLE skills(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, question_prompt TEXT, answer TEXT, confidence TEXT CHECK( confidence IN ('1','2','3') ))
+`);
 
 }
 let sql = `INSERT INTO interviews (interview_date, job_title,company, tech_stack, resume_version, interest_level, questions, notes) VALUES (?,?,?,?,?,?,?,?)`;
@@ -43,14 +45,25 @@ interviewDB.run(sql, [
   'No :)',
 ]);
 
-const allInterviews = `SELECT * FROM interviews`
+let sqlSkill = `INSERT INTO skills(name, question_prompt, answer, confidence) VALUES (?,?,?,?)`;
+const skillArray = ['React', 'Why would you use React?', 'Make reusable JSX components', '1']
 
+interviewDB.run(sqlSkill, skillArray)
+
+const allInterviews = `SELECT * FROM interviews`
+const allSkills = `SELECT * FROM skills`;
 interviewDB.all(allInterviews, [], (err,rows)=> {
   rows.forEach((row)=>{
-    console.log(row);
+    console.log("INTERVIEWS =>",row);
   })
 }
 )
+
+interviewDB.all(allSkills, [], (err, rows) => {
+  rows.forEach((row) => {
+    console.log("SKILLS =>",row);
+  });
+});
 
 interviewDB.close((err)=> {
     if(err) return console.log(err.message);
