bb665ee7c07063655775028e9784fd877e8902ec
chrisjamali <jamalichristopher@gmail.com>
2022-09-14 08:45:17 -0700

    error with interToSkill

--- server/models/interviewModel.js
+++ server/models/interviewModel.js
@@ -1,10 +1,9 @@
-
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
-const fs=require( 'fs');
-const makeDatabaseDir = require ("../utils/utils")
+const fs = require('fs');
+const makeDatabaseDir = require('../utils/utils');
 
-makeDatabaseDir()
+makeDatabaseDir();
 
 // Captures stack traces when handling queries. Makes it easier to debug errors resulting from
 // bad queries.
@@ -12,28 +11,39 @@ sqlite3.verbose();
 
 const dbPath = path.resolve(__dirname, '../../database/interviewDB.db');
 const dbExists = fs.existsSync(dbPath);
-
-const interviewDB = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
+//make the databases
+const interviewDB = new sqlite3.Database(
+  dbPath,
+  sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
   (err) => {
     // Console logs to confirm connection to the database
     if (err) {
-      throw (err);
+      throw err;
     }
     console.log('Connection successful to database :)');
-  },
-
-)
-if (!dbExists){
-interviewDB.run(
-  `CREATE TABLE interviews(id INTEGER PRIMARY KEY AUTOINCREMENT , interview_date ,job_title TEXT NOT NULL , company TEXT NOT NULL,tech_stack TEXT , resume_version TEXT, interest_level TEXT CHECK( interest_level IN ('1','2','3') )  , questions TEXT DEFAULT NULL, notes text DEFAULT NULL )`
+  }
 );
-
-interviewDB.run(`
+//make tables only if the databases wasnt created yet
+if (!dbExists) {
+  //make interviews table
+  interviewDB.run(
+    `CREATE TABLE interviews(id INTEGER PRIMARY KEY AUTOINCREMENT , interview_date ,job_title TEXT NOT NULL , company TEXT NOT NULL,tech_stack TEXT , resume_version TEXT, interest_level TEXT CHECK( interest_level IN ('1','2','3') )  , questions TEXT DEFAULT NULL, notes text DEFAULT NULL )`
+  );
+  //make skills table
+  interviewDB.run(`
 CREATE TABLE skills(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, question_prompt TEXT, answer TEXT, confidence TEXT CHECK( confidence IN ('1','2','3') ))
 `);
 
+//assocative entity or join table
+interviewDB.run(
+  `CREATE TABLE interToSkill (interview_id INTEGER NOT NULL, skill_id INTEGER NOT NULL, PRIMARY KEY(interview_id, skill_id) )`
+);
+ 
 }
+
+ //sample insert into interviews
 let sql = `INSERT INTO interviews (interview_date, job_title,company, tech_stack, resume_version, interest_level, questions, notes) VALUES (?,?,?,?,?,?,?,?)`;
+
 interviewDB.run(sql, [
   '9/13/2022',
   'Software Engineer',
@@ -45,8 +55,8 @@ interviewDB.run(sql, [
   'No :)',
 ]);
 
+//sample insert into skills
 let sqlSkill = `INSERT INTO skills(name, question_prompt, answer, confidence) VALUES (?,?,?,?)`;
-const skillArray = ['React', 'Why would you use React?', 'Make reusable JSX components', '1']
 
 interviewDB.run(sqlSkill, [
   'React',
@@ -55,21 +65,36 @@ interviewDB.run(sqlSkill, [
   '1',
 ]);
 
-const allInterviews = `SELECT * FROM interviews`
-const allSkills = `SELECT * FROM skills`;
-interviewDB.all(allInterviews, [], (err,rows)=> {
-  rows.forEach((row)=>{
-    console.log("INTERVIEWS =>",row);
-  })
-}
-)
+let joinInsert = `INSERT INTO interToSkill(skill_id,interview_id) VALUES(?,?)`
 
-interviewDB.all(allSkills, [], (err, rows) => {
+interviewDB.run(joinInsert, [1,1])
+
+//selection queries to get all the info from intervies and skills table
+const allInterviews = `SELECT * FROM interviews`;
+const allSkills = `SELECT * FROM skills`;
+const allJoin = `SELECT * FROM interToSkill`;
+//console.log all the interviews
+interviewDB.all(allInterviews, [], (err, rows) => {
   rows.forEach((row) => {
-    console.log("SKILLS =>",row);
+    console.log('INTERVIEWS =>', row);
   });
 });
 
-interviewDB.close((err)=> {
-    if(err) return console.log(err.message);
-})
+//console.log all the skills
+interviewDB.all(allSkills, [], (err, rows) => {
+  rows.forEach((row) => {
+    console.log('SKILLS =>', row);
+  });
+});
+
+interviewDB.all(allJoin, [], (err, rows) => {
+  rows.forEach((row) => {
+    console.log('JOINS =>', row);
+  });
+});
+
+
+//best practices to close database
+interviewDB.close((err) => {
+  if (err) return console.log(err.message);
+});
