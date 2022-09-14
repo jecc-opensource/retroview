75aa2eeb6b17e4b1b29363fd09af858a64731273
Eli Muir <eli_muir@Elis-MacBook-Pro.local>
2022-09-14 11:32:06 -0400

    minor updates to client files

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,19 +1,6 @@
-<<<<<<< HEAD
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './app';
 import {Provider} from 'react-redux';
 import store from './store'
 import styles from '../styles/Home.module.css'
-=======
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.scss'
->>>>>>> dev
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
