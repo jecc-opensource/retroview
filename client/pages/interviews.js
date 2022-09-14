0eb399e94f17bb17bd5ed24ee8fc5a81590a5faf
jmok3634 <jmok3634@gmail.com>
2022-09-14 15:40:12 -0400

    updated frontend layout

--- client/pages/interviews.js
+++ client/pages/interviews.js
@@ -1,16 +1,21 @@
-import Card from "../components/Card/Card";
+import SkillBox from "../components/SkillBox/SkillBox";
 import styles from "../styles/Home.module.scss";
+import Card from '../components/Card/Card';
 
 const Interviews = () => {
   const interviewArray = ['Spotify', 'Meta', 'Amazon', 'Discord', 'Netflix'];
   const newArr = interviewArray.map((interview) => {
-    return <Card key={interview} title={interview}></Card>
+    return <SkillBox key={interview} label={interview}></SkillBox>
   })
+
+  const handleClick = (e) => {
+    console.log('clicked')
+  }
 
   return (
     <>
     <h1 className={styles.title}>Interviews</h1>
-    <button>Add Interview</button>
+    <button onClick={handleClick}>Add Interview</button>
     <br></br>
     <div className={styles.interviewContainer}>
       {newArr}
