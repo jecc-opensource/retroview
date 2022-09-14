b14907788e939442ef60d55fa225d32dbb32838b
Eli Muir <eli_muir@Elis-MacBook-Pro.local>
2022-09-14 11:05:03 -0400

    minor updates

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MyApp from './_app';
+import App from './app';
 import {Provider} from 'react-redux';
-import store from './redux/store'
+import store from './store'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-   
-    </div>
-  )
-}
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
--- client/pages/store.js
+++ client/pages/store.js
@@ -1,3 +1,10 @@
-import { createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import reducers from './reducers/index';
+import { configureStore } from '@reduxjs/toolkit'
+import interviewReducer from './slice_store/interviewSlice'
+import skillReducer from './slice_store/skillSlice'
+
+export const store = configureStore({
+  reducer: {
+    interview: interviewReducer,
+    skill: skillReducer
+  },
+})
