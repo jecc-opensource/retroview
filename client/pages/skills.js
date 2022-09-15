4be1996d7cc5fc3a3fa35cbeef903ae6397cac93
Chase Walters <cwwalters@fastmail.com>
2022-09-14 18:01:59 -0600

    Use state for skills page, add super basic modal as demo

--- client/pages/skills.js
+++ client/pages/skills.js
@@ -1,20 +1,33 @@
 import SkillBox from "../components/SkillBox/SkillBox";
 import styles from '../styles/Home.module.scss';
+import { useSelector, useDispatch } from 'react-redux';
+import SkillModal from "../components/SkillModal/SkillModal";
+import { renderModal } from "../redux/slices/skillSlice";
 
 const Skills = () => {
-  const skillsArray = ['React', 'Express', 'Jest', 'React Testing Library', 'SQL', 'NoSQL', 'Skill1', 'Skill2', 'skill3'];
-  const newArr = skillsArray.map((skill) => {
-    return <SkillBox key={skill} label={skill} confidence={2}></SkillBox>
+  const dispatch = useDispatch();
+  const skillList = useSelector((state) => state.skill.skillList);
+  const modalSkill = useSelector((state) => state.skill.modalSkill);
+  const newArr = skillList.map((skill, index) => {
+    return (
+      <SkillBox 
+        label={skill.name}
+        confidence={skill.confidence}
+        key={`skillBox-${index}`}
+        onClick={() => dispatch(renderModal(skill.id))}
+      />
+    ) 
   })
 
   return (
     <>
     <h1 className={styles.title}>Skills</h1>
     <button>Add a skill</button>
-    <br></br>
+    <br/>
     <div className={styles.skillsContainer}>
         {newArr}
     </div>
+    { modalSkill && <SkillModal skillId={modalSkill} /> }
     </>
   )
 }
