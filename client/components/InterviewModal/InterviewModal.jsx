34a880cd545265d84630a11932d811c5dd16cef9
Chase W <cwwalters@fastmail.com>
2022-09-20 17:44:39 -0600

    Update InterviewModal.jsx

--- client/components/InterviewModal/InterviewModal.jsx
+++ client/components/InterviewModal/InterviewModal.jsx
@@ -22,10 +22,10 @@ const InterviewModal = ({ interviewId }) => {
         <br />
         <strong>Job Title: </strong>{interview.job_title}
         <br />
-        <strong>Tech Stack: </strong>{interview.tech_stack.map((tech, index) => {return (<li className={styles.techList}>{`${index + 1}. ${tech} `}</li>)})}
+        <strong>Tech Stack: </strong>{interview.tech_stack.map((tech, index) => {return (<li key={`tech-${index}`} className={styles.techList}>{`${index + 1}. ${tech} `}</li>)})}
         <strong>Résumé: </strong>{interview.resume_version}
         <br />
-        <strong>Questions: </strong>{interview.questions.map((question, index) => {return (<li className={styles.questList}>{`${index + 1}. ${question} `}</li>)})}
+        <strong>Questions: </strong>{interview.questions.map((question, index) => {return (<li key={`question-${index}`} className={styles.questList}>{`${index + 1}. ${question} `}</li>)})}
         <strong>Notes: </strong>{interview.notes}
         <hr />
         <InterviewLinkList />
